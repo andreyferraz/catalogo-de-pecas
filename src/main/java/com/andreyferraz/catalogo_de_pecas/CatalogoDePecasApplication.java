db19d3078a1f65354dda7ce5472aeb2135efef00
andreyferraz <andreyferraz@unimedceara.com.br>
2025-01-12 21:37:49 -0300

    primeiro commit

---
+++ src/main/java/com/andreyferraz/catalogo_de_pecas/CatalogoDePecasApplication.java
@@ -0,0 +1,13 @@
+package com.andreyferraz.catalogo_de_pecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoDePecasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoDePecasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andreyferraz/catalogo_de_pecas/CatalogoDePecasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreyferraz.catalogo_de_pecas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoDePecasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
