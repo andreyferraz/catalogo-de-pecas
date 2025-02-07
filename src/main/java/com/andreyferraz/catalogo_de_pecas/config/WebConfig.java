068c94807d6d3f1b2f692e4ae2ef2e4e503ae722
andreyferraz <andreyferraz@unimedceara.com.br>
2025-02-07 10:46:44 -0300

    configurando para salvar imagens local e no servidor

--- src/main/java/com/andreyferraz/catalogo_de_pecas/config/WebConfig.java
+++ src/main/java/com/andreyferraz/catalogo_de_pecas/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.andreyferraz.catalogo_de_pecas.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,12 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${upload.dir}")
+    private String uploadDir;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Configura o diretório de uploads para ser acessível como recurso estático
-        registry.addResourceHandler("/uploads/**") // Caminho acessível pela aplicação
-                .addResourceLocations("file:C:/uploads/") 
-                .setCachePeriod(3600); // Define tempo de cache (opcional, em segundos)
-    }
+        String resourceLocation = "file:" + uploadDir + "/";
 
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations(resourceLocation)
+                .setCachePeriod(3600); 
+    }
 }
